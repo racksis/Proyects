f13fc7c2f443a1e57e998e1d2a24678df929e8ca
racksis <58745314+racksis@users.noreply.github.com>
2019-12-14 18:33:19 -0600

    Add files via upload

---
+++ configdb.js
@@ -0,0 +1,8 @@
+module.exports = {
+   database: {
+       connectionLimit: 10,
+       host:'tlatoanicloud.com',
+       user:'tlatoani_udl',
+       password:'Udl_2019+',
+       database:'tlatoani_swiftdb'
+   }};
