fbc19ad0af10f7baa6a07a1e72d41e29e9638f14
racksis <58745314+racksis@users.noreply.github.com>
2019-12-14 18:44:47 -0600

    Delete configdb.js

--- configdb.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-   database: {
-       connectionLimit: 10,
-       host:'tlatoanicloud.com',
-       user:'tlatoani_udl',
-       password:'Udl_2019+',
-       database:'tlatoani_swiftdb'
-   }};
